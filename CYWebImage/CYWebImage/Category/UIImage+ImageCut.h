9d0798277c78a5fcac514957350b51d784d62d53
dongzb <dzb8818082@163.com>
2016-03-22 18:44:28 +0800

    修改了一写

--- CYWebImage/CYWebImage/Category/UIImage+ImageCut.h
+++ CYWebImage/CYWebImage/Category/UIImage+ImageCut.h
@@ -10,20 +10,13 @@
 NS_ASSUME_NONNULL_BEGIN
 @interface UIImage (ImageCut)
 
-//截取部分图像
--(UIImage*)getSubImage:(CGSize)size;
 
-+ (CGSize)getScaleImageFromSize:(CGSize)fromSize width:(CGFloat)fromW;
-/// 对图片尺寸进行压缩
--(UIImage*)scaledToSize:(CGSize)newSize;
-/**
- *  获取内存中已经缓存的图片 并根据限制的宽度 进行等比例缩放
- */
-+ (CGSize)getCacheImageForkey:(NSString*)imageUrl andRestrict:(CGFloat)width;
 /**
  *  将图片裁剪成圆形图片
  *
  */
 - (UIImage*)cirleImage;
+
+
 @end
 NS_ASSUME_NONNULL_END
--- CYWebImage/CYWebImage/Category/UIImage+ImageCut.m
+++ CYWebImage/CYWebImage/Category/UIImage+ImageCut.m
@@ -9,56 +9,7 @@
 #import "UIImage+ImageCut.h"
 #import "SDImageCache.h"
 @implementation UIImage (ImageCut)
-//截取部分图像
--(UIImage*)getSubImage:(CGSize)size
-{
-    CGRect rect = CGRectMake(self.size.width*(1-size.width), self.size.height*(1-size.height), self.size.width*size.width, self.size.height*size.height);
-    CGImageRef subImageRef = CGImageCreateWithImageInRect(self.CGImage, rect);
-    CGRect smallBounds = CGRectMake(0, 0, CGImageGetWidth(subImageRef), CGImageGetHeight(subImageRef));
-    UIGraphicsBeginImageContext(smallBounds.size);
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    CGContextDrawImage(context, smallBounds, subImageRef);
-    UIImage* smallImage = [UIImage imageWithCGImage:subImageRef];
-    UIGraphicsEndImageContext();
-    return smallImage;
-}
 
-+ (CGSize)getScaleImageFromSize:(CGSize)fromSize width:(CGFloat)fromW
-{
-    CGFloat width = fromSize.width;
-    CGFloat height = fromSize.height;
-    CGFloat scale = height/width;
-    height = fromW *scale;
-    NSString *str = [NSString stringWithFormat:@"%f",height];
-    if (str.integerValue ==0) {
-        height = 0;
-    }
-    return CGSizeMake(fromW, height);
-}
-//对图片尺寸进行压缩--
--(UIImage*)scaledToSize:(CGSize)newSize
-{
-    // Create a graphics image context
-    UIGraphicsBeginImageContext(newSize);
-    // Tell the old image to draw in this new context, with the desired
-    // new size
-    [self drawInRect:CGRectMake(0,0,newSize.width,newSize.height)];
-    // Get the new image from the context
-    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
-    // End the context
-    UIGraphicsEndImageContext();
-    // Return the new image.
-    return newImage;
-}
-
-/**
- *  获取内存中已经缓存的图片 并根据限制的宽度 进行等比例缩放
- */
-+ (CGSize)getCacheImageForkey:(NSString*)imageUrl andRestrict:(CGFloat)width
-{
-    UIImage *coverImage = [[SDImageCache sharedImageCache] imageFromDiskCacheForKey:imageUrl];
-    return [UIImage getScaleImageFromSize:coverImage.size width:width];
-}
 /**
  *  将图片裁剪成圆形图片
  *
